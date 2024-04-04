3d708f6fa77f2315279d478a729adb269af43a47
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2024-04-04 00:45:39 -0400

    Update Go dependencies (#19)

--- sb/tpl/internal/go_templates/texttemplate/link_test.go
+++ sb/tpl/internal/go_templates/texttemplate/link_test.go
@@ -2,18 +2,16 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build go1.13
-// +build go1.13
-
 package template_test
 
 import (
 	"bytes"
-	"github.com/strawberry-tools/strawberry/tpl/internal/go_templates/testenv"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
+
+	"github.com/strawberry-tools/strawberry/tpl/internal/go_templates/testenv"
 )
 
 // Issue 36021: verify that text/template doesn't prevent the linker from removing
